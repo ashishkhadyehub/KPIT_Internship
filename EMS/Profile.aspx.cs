f1a44a2a58ad8aa0a3a5c5d3f7393993f442a62b
ashishkhadye33 <ashishkhadye33@gmail.com>
2025-07-02 14:41:00 +0530

    dotnet project day 1

---
+++ EMS/Profile.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EMS
+{
+    public partial class Profile : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["connstr"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(Session["EmployeeId"]!=null)
+            {
+                if(!IsPostBack)
+                {
+                    getData();
+                }
+                
+            }
+            else
+            {
+                Response.Redirect("Login.aspx");
+            }
+        }
+
+        protected void getData()
+        {
+            con.Close();
+            SqlCommand cmd = new SqlCommand("select * from Employees where Srno=@id ",con);
+            cmd.Parameters.AddWithValue("@id", Session["EmployeeId"]);
+            con.Open();
+            SqlDataReader sdr = cmd.ExecuteReader();
+            if(sdr.HasRows)
+            {
+                sdr.Read();
+                txtName.Text = sdr.GetValue(1).ToString();
+                txtContact.Text = sdr.GetValue(2).ToString();
+                txtEmail.Text = sdr.GetValue(3).ToString();
+                txtdept.Text = sdr.GetValue(4).ToString();
+
+            }
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            con.Close();
+            SqlCommand cmd = new SqlCommand("update Employees set Name=@name,Contact=@contact,Email=@email where Srno=@id", con);
+            cmd.Parameters.AddWithValue("@name", txtName.Text);
+            cmd.Parameters.AddWithValue("@contact", txtContact.Text);
+            cmd.Parameters.AddWithValue("@email", txtEmail.Text);
+            cmd.Parameters.AddWithValue("@id", Session["EmployeeId"]);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            getData();
+
+        }
+    }
+}
---
+++ EMS/Register.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EMS
+{
+    public partial class Register : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["connstr"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            con.Close();
+            SqlCommand cmd = new SqlCommand("insert into Employees values (@name,@contact,@email,@dept,@pwd)",con);
+            cmd.Parameters.AddWithValue("@name", txtName.Text);
+            cmd.Parameters.AddWithValue("@contact", txtContact.Text);
+            cmd.Parameters.AddWithValue("@email", txtEmail.Text);
+            cmd.Parameters.AddWithValue("@dept", ddldept.SelectedItem.Text);
+            cmd.Parameters.AddWithValue("@pwd", txtPassword.Text);
+            con.Open();
+            cmd.ExecuteNonQuery();
+
+            txtContact.Text = "";
+            txtEmail.Text = "";
+            txtName.Text = "";
+            txtPassword.Text = "";
+            ddldept.SelectedIndex = 0;
+
+            this.ClientScript.RegisterStartupScript(this.GetType(), "SweetAlert", "swal('Registered Successfully..!','','success');", true);
+        }
+    }
+}
